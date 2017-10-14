c40752f347751b86345203ce2435fef725012aa4
Jeremiah Lowhorn <jlowhorn39@yahoo.com>
2017-10-14 09:07:58 -0400

    Create MondayRun.R

---
+++ MondayRun.R
@@ -0,0 +1,11 @@
+library(dplyr)
+library(shiny)
+library(shinydashboard)
+library(stringr)
+library(tidyr)
+library(rpivotTable)
+library(data.table)
+
+
+setwd("//KNX3IT/AWG Management/RGR/Activation Needs Dashboard/Application File")
+runApp(host="0.0.0.0",port=5070)
